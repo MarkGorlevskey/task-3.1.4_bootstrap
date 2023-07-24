09b2390a69fd85495410cc0d7f37d737a5c62831
MarkGorlevskey <124619894+MarkGorlevskey@users.noreply.github.com>
2023-07-24 19:25:29 +0300

    Update AdminController.java

--- PP_3_1_4_Bootstrap/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ PP_3_1_4_Bootstrap/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -23,16 +22,10 @@ public class AdminController {
         this.roleService = roleService;
     }
 
-    @GetMapping
-    public String getAllUsers(Model model, Principal principal) {
-
-        User customer = userService.findByUsername(principal.getName());
-        List<Role> allRoles = roleService.getRoles();
-        List<User> allCustomers = userService.getAllUsers();
-        model.addAttribute("customer", customer);
-        model.addAttribute("allCustomers", allCustomers);
-        model.addAttribute("allPossibleRoles", allRoles);
-        model.addAttribute("emptyUser", new User());
+    @GetMapping()
+    public String admin(Principal principal, Model model) {
+        List<User> userList = userService.getAllUsers();
+        model.addAttribute("userList", userList);
         return "admin";
     }
 
@@ -50,6 +43,7 @@ public class AdminController {
     public String showCreateUserForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
+        model.addAttribute("roles", roleService.getRoles());
         return "new_user";
     }
 
