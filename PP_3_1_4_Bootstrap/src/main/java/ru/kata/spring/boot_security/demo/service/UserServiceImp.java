75a2c744121d65d9220ea1ffe0f1dfd870538d60
MarkGorlevskey <124619894+MarkGorlevskey@users.noreply.github.com>
2023-07-24 19:27:23 +0300

    Update UserServiceImp.java

--- PP_3_1_4_Bootstrap/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
+++ PP_3_1_4_Bootstrap/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.configs.WebSecurityConfig;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
@@ -28,6 +29,7 @@ public class UserServiceImp implements UserService {
     @Transactional
     @Override
     public void save(User user) {
+        user.setPassword(WebSecurityConfig.passwordEncoder().encode(user.getPassword()));
         userRepository.save(user);
     }
 
